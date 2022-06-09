6a7d912d1e0cbc10eb239bd21865c6d2e430bda1
Debora Lima <104090687+imdebora@users.noreply.github.com>
2022-06-09 16:20:36 -0300

    exercicios

--- exercicios/PHP_2/e_php29B.php
+++ exercicios/PHP_2/e_php29B.php
@@ -4,7 +4,7 @@
 // Cabecalho: 
 // Itens:
 
-$sacola1 = array('item1' => 'pera', 'item2' => 'banana', 'item3' => 'cafe', 'item4' => 'cebola');
+$sacola1 = array('item1' => 'Pera', 'item2' => 'banana', 'item3' => 'cafe', 'item4' => 'cebola');
 $sacola2 = array('item1' => 'matambre', 'item2' => 'parmesao', 'item3' => 'mussa', 'item4' => 'temperos');
 $sacola3 = array('item1' => 'carvao', 'item2' => 'sal', 'item3' => 'oliva', 'item4' => 'cheiroverde');
 $sacola4 = array('item1' => 'vodka', 'item2' => 'gelo', 'item3' => 'limao', 'item4' => 'cravo');
@@ -12,14 +12,17 @@ $br = "<br><br>";
 $caixa1 = array(1 => $sacola1);
 $caixa2 = array(2 => $sacola2, 2 => $sacola3, 3 => $sacola4);
 $carro = array('malas' => $caixa1, 'banco' => $caixa2);
-echo "===========<br>COMPRAS<br>===========<br>" ;
+echo "========================<br>COMPRAS DO PORTA MALAS<br>========================<br>" ;
 
-foreach ($carro as $x){
-    // echo "<br> Chave: $x";
-    foreach ($x as $itens ){
-        foreach($itens as $churras){
-        echo $churras . $br;
-        }
+foreach($carro['malas'] as $caixas) {
+    foreach ($caixas as $itens) {
+        echo $itens . $br;
+    }
+}
+echo "====================<br>COMPRAS DO BANCO<br>====================<br>" ;
+foreach($carro['banco'] as $caixas) {
+    foreach ($caixas as $itens) {
+        echo $itens . $br;
     }
 }
 ?>
